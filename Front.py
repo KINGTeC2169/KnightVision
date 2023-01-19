dc42b40683ccc13be3d8345b1aab87706f322ba7
jakeahorner <jakeahorner@gmail.com>
2023-01-18 18:31:07 -0600

    added threading

--- Front.py
+++ Front.py
@@ -3,7 +3,10 @@ import cv2 as cv
 import time
 import logging
 from networktables import NetworkTables
-cap = cv.VideoCapture(0)
+import threading
+cap = cv.VideoCapture(1)
+cap.set(3,1280)
+cap.set(4,720)
 
 NetworkTables.startClientTeam(2169)
 NetworkTables.initialize(server= "10.21.69.2")
@@ -11,6 +14,35 @@ time.sleep(5)
 
 sd = NetworkTables.getTable("SmartDashboard")
 logging.basicConfig(level=logging.DEBUG)
+
+def findObjects(img, name):
+    contours, heiarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+    if len(contours) != 0:
+        cnt = max(contours, key = cv.contourArea)
+        if cv.contourArea(cnt) > 50:
+            rows,cols = img.shape[:2]
+            [vx,vy,x,y] = cv.fitLine(cnt, cv.DIST_L2,0,0.01,0.01)
+
+        
+            M = cv.moments(cnt)
+            try:
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                img = cv.circle(img, [cx,cy], 5, [100,90,90], 2)
+                sd.putNumberArray("center", [cx,cy])
+
+            except ZeroDivisionError:
+                print('balls')
+
+            # I put this here because I was having issuse with the slope being undefined when straight up
+            if vx > 0:
+                lefty = int((-x*vy/vx) + y)
+                righty = int(((cols-x)*vy/vx)+y)
+                sd.putNumber("angle", (np.arctan(vy/vx)* 180) / np.pi)
+                img = cv.line(img,(cols-1,righty),(0,lefty),(150,100,40),2)
+    cv.imshow(name, img)
+
+
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
@@ -24,28 +56,10 @@ while True:
 
     img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     img = cv.GaussianBlur(img,(5,5),0)
-    img = cv.inRange(img,np.array([10,90,90]),np.array([43,255,255]))
-    contours, heiarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-    if len(contours) != 0:
-        cnt = max(contours, key = cv.contourArea)
-        rows,cols = img.shape[:2]
-        [vx,vy,x,y] = cv.fitLine(cnt, cv.DIST_L2,0,0.01,0.01)
-
-        M = cv.moments(cnt)
-        cx = int(M['m10']/M['m00'])
-        cy = int(M['m01']/M['m00'])
-
-        img = cv.circle(img, [cx,cy], 5, [100,90,90], 2)
-        # I put this here because I was having issuse with the slope being undefined when straight up
-        if vx > 0:
-            lefty = int((-x*vy/vx) + y)
-            righty = int(((cols-x)*vy/vx)+y)
-            sd.putNumber("angle", (np.arctan(vy/vx)* 180) / np.pi)
-            sd.putNumberArray("center", [cx,cy])
-            img = cv.line(img,(cols-1,righty),(0,lefty),(150,100,40),2)
-    cv.imshow("Best Fit", img)
-    
-
+    yellowImg = cv.inRange(img,np.array([15,191,90]),np.array([33,255,255]))
+    purpleImg = cv.inRange(img,np.array([113,90,110]),np.array([131,255,255]))
+    yellowThread = threading.Thread(target=findObjects(yellowImg, "yellow"))
+    purpleThread = threading.Thread(target=findObjects(purpleImg, "purple"))
     print(NetworkTables.isConnected())
         
 
--- Palm.py
+++ Palm.py
@@ -4,7 +4,9 @@ import time
 import logging
 from networktables import NetworkTables
 import threading
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture(1)
+cap.set(3,1280)
+cap.set(4,720)
 
 NetworkTables.startClientTeam(2169)
 NetworkTables.initialize(server= "10.21.69.2")
@@ -13,33 +15,34 @@ time.sleep(5)
 sd = NetworkTables.getTable("SmartDashboard")
 logging.basicConfig(level=logging.DEBUG)
 
-def findObjects(img):
+def findObjects(img, name):
     contours, heiarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
     if len(contours) != 0:
         cnt = max(contours, key = cv.contourArea)
-        rows,cols = img.shape[:2]
-        [vx,vy,x,y] = cv.fitLine(cnt, cv.DIST_L2,0,0.01,0.01)
+        if cv.contourArea(cnt) > 50:
+            rows,cols = img.shape[:2]
+            [vx,vy,x,y] = cv.fitLine(cnt, cv.DIST_L2,0,0.01,0.01)
 
         
-        M = cv.moments(cnt)
-        try:
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            img = cv.circle(img, [cx,cy], 5, [100,90,90], 2)
+            M = cv.moments(cnt)
+            try:
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                img = cv.circle(img, [cx,cy], 5, [100,90,90], 2)
+                sd.putNumberArray("center", [cx,cy])
 
-        except ZeroDivisionError:
-            print('balls')
+            except ZeroDivisionError:
+                print('balls')
 
-        # I put this here because I was having issuse with the slope being undefined when straight up
-        if vx > 0:
-            lefty = int((-x*vy/vx) + y)
-            righty = int(((cols-x)*vy/vx)+y)
-            sd.putNumber("angle", (np.arctan(vy/vx)* 180) / np.pi)
-            sd.putNumberArray("center", [cx,cy])
-            img = cv.line(img,(cols-1,righty),(0,lefty),(150,100,40),2)
-    return img
+            # I put this here because I was having issuse with the slope being undefined when straight up
+            if vx > 0:
+                lefty = int((-x*vy/vx) + y)
+                righty = int(((cols-x)*vy/vx)+y)
+                sd.putNumber("angle", (np.arctan(vy/vx)* 180) / np.pi)
+                img = cv.line(img,(cols-1,righty),(0,lefty),(150,100,40),2)
+    cv.imshow(name, img)
 
-    
+
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
@@ -53,15 +56,10 @@ while True:
 
     img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     img = cv.GaussianBlur(img,(5,5),0)
-    yellowImg = cv.inRange(img,np.array([10,90,90]),np.array([43,255,255]))
-    purpleImg = cv.inRange(img,np.array([10,90,90]),np.array([43,255,255]))
-    img = findObjects(yellowImg)
-
-
-    
-    cv.imshow("Best Fit", img)
-    
-
+    yellowImg = cv.inRange(img,np.array([15,191,90]),np.array([33,255,255]))
+    purpleImg = cv.inRange(img,np.array([113,90,110]),np.array([131,255,255]))
+    yellowThread = threading.Thread(target=findObjects(yellowImg, "yellow"))
+    purpleThread = threading.Thread(target=findObjects(purpleImg, "purple"))
     print(NetworkTables.isConnected())
         
 
