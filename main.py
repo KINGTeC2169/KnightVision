011e7f7296eb5897f37bde5960f24f79c068ce4a
jakeahorner <jakeahorner@gmail.com>
2023-02-01 15:54:13 -0600

    main

--- main.py
+++ main.py
@@ -17,8 +17,25 @@ at_detector = Detector(
 
 def openFeed(index):
     cap = cv.VideoCapture(int(index))
+    #cap.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc(*'MJPG'))
     while True:
         reta, img = cap.read()
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        #img = cv.inRange(img,np.array([100,100,100]),np.array([255,255,255]))
+        
+
+        det = at_detector.detect(img, estimate_tag_pose=True, camera_params=(1083.1843730953367,1070.1431886531207,586.9131989071315,293.5012883025358), tag_size=0.1524)
+        if len(det) > 0:
+            maxDet = det[0]
+            for i in det:
+                if (i.decision_margin > maxDet.decision_margin) and (0 < i.tag_id < 9):
+                    maxDet = i
+            
+            img = cv.circle(img, np.array(maxDet.corners.tolist()[0], dtype=np.int64), 10, [98,23,87], 5)
+            img = cv.circle(img, np.array(maxDet.corners.tolist()[1], dtype=np.int64), 10, [98,23,87], 5)
+            img = cv.circle(img, np.array(maxDet.corners.tolist()[2], dtype=np.int64), 10, [98,23,87], 5)
+            img = cv.circle(img, np.array(maxDet.corners.tolist()[3], dtype=np.int64), 10, [98,23,87], 5)
+            img = cv.circle(img, np.array(maxDet.center.tolist(), dtype=np.int64), 10, [98,23,87], 5)
         cv.imshow(str(index), img)
 
 
