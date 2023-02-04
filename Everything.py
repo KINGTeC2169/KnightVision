d88270cdae609f2fa58d2cffd709b556796789e6
jakeahorner <jakeahorner@gmail.com>
2023-02-04 13:57:23 -0600

    Cameras dont crash when not found, publishes apriltag data as well as camera status to networktable and deletes data if not updated. Fixed front apriltag feed

--- Everything.py
+++ Everything.py
@@ -9,7 +9,7 @@ from networktables import NetworkTables
 at_detector = Detector(
     families="tag16h5",
     nthreads=4,
-    quad_decimate=1.0,
+    quad_decimate=2.0,
     quad_sigma=0.0,
     refine_edges=1,
     decode_sharpening=0.25,
@@ -56,38 +56,42 @@ sd =  NetworkTables.getTable("SmartDashboard")
 
 def apriltag(img, name):
     
-
+    newValue = False
     det = at_detector.detect(img, estimate_tag_pose=True, camera_params=(1083.1843730953367,1070.1431886531207,586.9131989071315,293.5012883025358), tag_size=0.1524)
     if len(det) > 0:
         maxDet = det[0]
         for i in det:
-            if (i.decision_margin > maxDet.decision_margin) and (0 < i.tag_id < 9):
+            if (i.decision_margin > maxDet.decision_margin):
                 maxDet = i
-        
-        img = cv.circle(img, np.array(maxDet.corners.tolist()[0], dtype=np.int64), 10, [98,23,87], 5)
-        img = cv.circle(img, np.array(maxDet.corners.tolist()[1], dtype=np.int64), 10, [98,23,87], 5)
-        img = cv.circle(img, np.array(maxDet.corners.tolist()[2], dtype=np.int64), 10, [98,23,87], 5)
-        img = cv.circle(img, np.array(maxDet.corners.tolist()[3], dtype=np.int64), 10, [98,23,87], 5)
-        img = cv.circle(img, np.array(maxDet.center.tolist(), dtype=np.int64), 10, [98,23,87], 5)
-        pose_r = maxDet.pose_R
-        r11 = [0][0]
-        r12 = pose_r[0][1]
-        r13 = pose_r[0][2]
-        r21 = pose_r[1][0]
-        r22 = pose_r[1][1]
-        r23 = pose_r[1][2]
-        r31 = pose_r[2][0]
-        r32 = pose_r[2][1]
-        r33 = pose_r[2][2]
-        
-        AprilTagYaw = np.round(np.degrees(np.arctan(-r31/np.sqrt((r32 * r32)+(r33 * r33)))),3)
-        #AprilTagPitch = round(np.degrees(np.arctan(-r32/r33)),3)
-        #AprilTagRoll = round(np.degrees(np.arctan(r21/r11)),3)
-        imgColor = cv.putText(img, str(maxDet.tag_id), np.array(maxDet.center.tolist(), dtype=np.int64), font, 3, (100, 255, 0), 3, cv.LINE_AA)
+        if(0 < i.tag_id < 9):
+            img = cv.circle(img, np.array(maxDet.corners.tolist()[0], dtype=np.int64), 10, [98,23,87], 5)
+            img = cv.circle(img, np.array(maxDet.corners.tolist()[1], dtype=np.int64), 10, [98,23,87], 5)
+            img = cv.circle(img, np.array(maxDet.corners.tolist()[2], dtype=np.int64), 10, [98,23,87], 5)
+            img = cv.circle(img, np.array(maxDet.corners.tolist()[3], dtype=np.int64), 10, [98,23,87], 5)
+            img = cv.circle(img, np.array(maxDet.center.tolist(), dtype=np.int64), 10, [98,23,87], 5)
+            pose_r = maxDet.pose_R
+            pose_t = maxDet.pose_t[2] * 39.3701
+            r31 = pose_r[2][0]
+            r32 = pose_r[2][1]
+            r33 = pose_r[2][2]
+            
+            AprilTagYaw = np.round(np.degrees(np.arctan(-r31/np.sqrt((r32 * r32)+(r33 * r33)))),3)
+            newValue = True
+            sd.putNumber(name + "-apriltag-Yaw", AprilTagYaw)
+            sd.putNumber(name + "-apriltag-X", pose_t)
+            sd.putNumber(name + "-apriltag-Id", maxDet.tag_id)
+            #AprilTagPitch = round(np.degrees(np.arctan(-r32/r33)),3)
+            #AprilTagRoll = round(np.degrees(np.arctan(r21/r11)),3)
+            imgColor = cv.putText(img, str(maxDet.tag_id), np.array(maxDet.center.tolist(), dtype=np.int64), font, 3, (100, 255, 0), 3, cv.LINE_AA)
     cv.imshow(name, img)
+    if not newValue:
+        sd.delete(name + "-apriltag-Yaw")
+        sd.delete(name + "-apriltag-X")
+        sd.delete(name + "-apriltag-Id")
 
 
 def findObjects(img, name, index, camId):
+    newValue = False
     contours, heiarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
     if len(contours) != 0:
         cnt = max(contours, key = cv.contourArea)
@@ -99,14 +103,19 @@ def findObjects(img, name, index, camId):
                 cx = int(M['m10']/M['m00'])
                 cy = int(M['m01']/M['m00'])
                 img = cv.circle(img, [cx,cy], 5, [100,90,90], 2)
+                newValue = True
                 sd.putNumberArray(camId + name + "-Center", [cx,cy])
 
             except ZeroDivisionError:
                 print('balls')
 
     cv.imshow(name + " " + str(index), img)
+    if not newValue:
+        sd.delete(camId + name + "-Center")
+        
 
 def findObjWithLines(img, name, index, camId):
+    newValue = False
     contours, heiarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
     if len(contours) != 0:
         cnt = max(contours, key = cv.contourArea)
@@ -119,6 +128,7 @@ def findObjWithLines(img, name, index, camId):
                 cx = int(M['m10']/M['m00'])
                 cy = int(M['m01']/M['m00'])
                 img = cv.circle(img, [cx,cy], 5, [100,90,90], 2)
+                newValue = True
                 sd.putNumberArray(camId + name + "-Center", [cx,cy])
 
             except ZeroDivisionError:
@@ -131,6 +141,9 @@ def findObjWithLines(img, name, index, camId):
                 img = cv.line(img,(cols-1,righty),(0,lefty),(150,100,40),2)
                 
     cv.imshow(name + " " + str(index), img)
+    if not newValue:
+        sd.delete(camId + name + "-Center")
+        sd.delete("Palm-" + name + "-Angle")
 
 def cube(img, index, camId):
     img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
@@ -152,23 +165,43 @@ def cone(img, index, camId, hasLines):
 
 
 while True:
-    ret1, imgFront = frontCap.read()
-    ret2, imgPalm = palmCap.read()
-    ret3, imgAprilLeft = apriltagLeftCap.read()
-    ret4, imgAprilRight = apriltagRightCap.read()
+    if frontCap.isOpened():
+        ret1, imgFront = frontCap.read()
+        cone(imgFront, frontIndex, "Front-", False)
+        cube(imgFront, frontIndex, "Front-")
+        imgFront = cv.cvtColor(imgFront, cv.COLOR_BGR2GRAY)
+        imgFront = cv.inRange(imgFront, np.array([120]),np.array([255]))
+        apriltag(imgFront, "front")
+        sd.putBoolean("Front", True)
+    else:
+        sd.putBoolean("Front", False)
+    if palmCap.isOpened():
+        ret2, imgPalm = palmCap.read()
+        cone(imgPalm, palmIndex, "Palm-", True)
+        cube(imgPalm, palmIndex, "Palm-")
+        sd.putBoolean("Palm", True)
+    else:
+        sd.putBoolean("Palm", False)
+    if apriltagLeftCap.isOpened():
+        ret3, imgAprilLeft = apriltagLeftCap.read()
+        imgAprilLeft = cv.inRange(imgAprilLeft,np.array([100,100,100]),np.array([255,255,255]))
+        apriltag(imgAprilLeft, "left")
+        sd.putBoolean("Left", True)
+    else:
+        sd.putBoolean("Left", False)
+    if apriltagRightCap.isOpened():
+        ret4, imgAprilRight = apriltagRightCap.read()
+        imgAprilRight = cv.inRange(imgAprilRight,np.array([100,100,100]),np.array([255,255,255]))
+        apriltag(imgAprilRight, "right")
+        sd.putBoolean("Right", True)
+    else:
+        sd.putBoolean("Right", False)
 
-    imgAprilLeft = cv.inRange(imgAprilLeft,np.array([100,100,100]),np.array([255,255,255]))
-    imgAprilRight = cv.inRange(imgAprilRight,np.array([100,100,100]),np.array([255,255,255]))
-    apriltag(imgAprilLeft, "left")
-    apriltag(imgAprilRight, "right")
     
-    #cv.imshow("balls", imgAprilRight)
-    cone(imgFront, frontIndex, "Front-", False)
-    cube(imgFront, frontIndex, "Front-")
-    imgFront = cv.cvtColor(imgFront, cv.COLOR_BGR2GRAY)
-    apriltag(imgFront, "front")
-    cone(imgPalm, palmIndex, "Palm-", True)
-    cube(imgPalm, palmIndex, "Palm-")
+    
+    
+    
+    
 
     if cv.waitKey(1) == ord('q'):
             break
